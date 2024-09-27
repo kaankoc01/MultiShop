a461bcd41c5ea92ddedb7e4c0bef944d36d75c64
kaan koc <kaankoc01@gmail.com>
2024-09-27 17:30:50 +0300

    Alışveriş Sepeti Sayfası Tasarımı commit

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ContactVİewComponents/_ContactDetailComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ContactVİewComponents
+{
+    public class _ContactDetailComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+        return View(); }
+    }
+}
