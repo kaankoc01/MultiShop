00d3a5fdfff10a91e3d61911140ab4ebd760c39b
kaan koc <kaankoc01@gmail.com>
2024-09-30 18:47:57 +0300

    Admin Teması commit

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
+    [AllowAnonymous]
+    [Route("Admin/Category")]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -15,8 +17,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
-        [AllowAnonymous]
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Ana Sayfa";
@@ -24,16 +25,119 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             ViewBag.v3 = "Kategori Listesi";
             ViewBag.v0 = "Kategori İşlemleri";
 
-            var client = _httpClientFactory.CreateClient();
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+
             var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData); // listelerken deserialize
                 return View(values);
             }
 
             return View();
         }
+        [HttpGet]
+        [Route("CreateCategory")]
+        public IActionResult CreateCategory()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Yeni Kategori Girişi";
+            ViewBag.v0 = "Kategori İşlemleri";
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("CreateCategory")]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+            var jsondata = JsonConvert.SerializeObject(createCategoryDto); //eklerken serialize
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("DeleteCategory/{id}")]
+        
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+            var responseMessage = await client.DeleteAsync("https://localhost:7070/api/Categories?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Güncelleme Sayfası";
+            ViewBag.v0 = "Kategori İşlemleri";
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto> (jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+        [Route("UpdateCategory/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7070/api/Categories/" , stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+
+            }
+            return View();
+        }
     }
+
 }
+
