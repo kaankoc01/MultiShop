58865f3992980c15abdbdb3d7f19168e6a45e86b
kaan koc <kaankoc01@gmail.com>
2024-10-02 01:21:04 +0300

    Öne çıkan ürünler

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductDefaultComponentPartial.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureProductDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
 
-        return View(); 
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData); // listelerken deserialize
+                return View(values);
+            }
+            return View(); 
         
         }
     }
--- Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/GetByIdCategoryDto.cs
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -4,6 +4,7 @@
     {
         public string CategoryId { get; set; }
         public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
     }
 
 }
