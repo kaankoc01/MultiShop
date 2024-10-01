273884a74aa431ad0a4b9a61a7dfcdc4aa69382a
kaan koc <kaankoc01@gmail.com>
2024-10-01 21:48:13 +0300

    Öne çıkan alan commit

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,11 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+       
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            // HttpClientHandler ile SSL doğrulamasını atlıyoruz.
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // IHttpClientFactory kullanarak client'ı oluşturuyoruz ve custom handler ekliyoruz.
+            var client = new HttpClient(clientHandler);
+
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData); // listelerken deserialize
+                return View(values);
+            }
             return View();
         }
     }
