31934abd445c2b0c9544ffb83321f8e9aed043ec
kaan koc <kaankoc01@gmail.com>
2024-10-12 02:51:27 +0300

    Kredi Kartı front-end tamamlandı

---
+++ Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Directory1 = "MultiShop";
+            ViewBag.Directory2 = "Ödeme Ekranı";
+            ViewBag.Directory3 = "Kartla Ödeme";
+            return View();
+        }
+    }
+}
